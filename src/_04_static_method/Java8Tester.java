8271117159badd9123f3d6940cc97943a894dff6
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2023-01-23 09:25:11 +0300

    static method

---
+++ src/_04_static_method/Java8Tester.java
@@ -0,0 +1,9 @@
+package _04_static_method;
+
+public class Java8Tester {
+
+    public static void main(String[] args) {
+
+        Vehicle.print();
+    }
+}
---
+++ src/_04_static_method/Vehicle.java
@@ -0,0 +1,9 @@
+package _04_static_method;
+
+public interface Vehicle {
+
+    static void print() {
+        System.out.println("I am a vehicle!");
+    }
+
+}
