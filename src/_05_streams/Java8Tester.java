08dccab82d98b1488d18e4be39ab10fbdf6d9624
hüseyin <hsaglam001@gmail.com>
2023-01-25 23:32:03 +0300

    Feature/stream (#4)
    * stream example
    
    * stream example
    
    Co-authored-by: ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>

--- src/_05_streams/Java8Tester.java
+++ src/_05_streams/Java8Tester.java
@@ -1,6 +1,7 @@
 package _05_streams;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -50,5 +51,18 @@ public class Java8Tester {
         *  ardından forEach işlemini koşturmaktadır.
         * */
 
+        System.out.println("******************* Collectors ***************************");
+        List<String> string3 = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        String mergedString = string3.stream().filter(string -> !string.isEmpty()).collect(Collectors.joining(", "));
+        System.out.println("Merged String: " + mergedString);
+
+        System.out.println("******************* Statistics ***************************");
+        List numberss = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        IntSummaryStatistics stats = numberss.stream().mapToInt((y) -> (int) y).summaryStatistics();
+
+        System.out.println("Highest number in List : " + stats.getMax());
+        System.out.println("Lowest number in List : " + stats.getMin());
+        System.out.println("Sum of all numbers : " + stats.getSum());
+        System.out.println("Average of all numbers : " + stats.getAverage());
     }
 }
