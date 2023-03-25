95f7e9d6c963768749e894508d2936903b3e396a
hüseyin <hsaglam001@gmail.com>
2023-03-26 01:28:53 +0300

    11 function programming (#14)
    * Java 8 Predicate Examples
    
    * Java 8 BiPredicate Examples
    
    * Java 8 Consumer examples
    
    * Java 8 BiConsumer examples
    
    * Java 8 Consumer examples
    
    ---------
    
    Co-authored-by: ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>

---
+++ src/_11_function_programming/_08_consumer/Java8Tester.java
@@ -0,0 +1,26 @@
+package _11_function_programming._08_consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Java8Tester {
+
+    public static void main(String[] args) {
+
+        // Consumer<T> 		void accept(T t)
+        Consumer<String> consumer = x -> System.out.println(x);
+        consumer.accept("Selam");
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        getAll(list,x -> System.out.println(x));
+
+        Consumer<Integer> consumer1 = (Integer x) -> System.out.println(x);
+        getAll(list,consumer1);
+    }
+    public static <T> void getAll(List<T> list, Consumer<T> consumer){
+        for (T t : list){
+            consumer.accept(t);
+        }
+    }
+}
